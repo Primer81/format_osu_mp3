b3afbe31be6313a83c172a2e0f52b625fc81d3d0
Grahm Larkham <gwlarkham@gmail.com>
2018-02-13 21:20:08 -0500

    Latest and greatest

--- format_osu_mp3.py
+++ format_osu_mp3.py
@@ -2,20 +2,23 @@
 
 import argparse
 import os
-import urllib.request
-
-import eyed3.id3
+import shutil
+import eyed3
 import logging
 import zipfile
+import json
 
 from shutil import copyfile
 from enum import Enum
+from urllib import request, error as url_error
+from eyed3 import id3
 
 logging.getLogger("eyed3.mp3.headers").setLevel(logging.CRITICAL)
 
-class Keys(Enum):
-    VERSION         = 1
-    AUDIO_FILENAME  = 2
+class KEYS(Enum):
+    VERSION         = 0
+    AUDIO_FILENAME  = 1
+    MODE            = 2
     TITLE           = 3
     TITLE_UNICODE   = 4
     ARTIST          = 5
@@ -26,35 +29,57 @@ class Keys(Enum):
     BEATMAP_SET_ID  = 10
     BG_FILENAME     = 11
 
-def is_osz_zip(zip_name):
-    return zip_name[:-4] == ".osz"
+GENRE_DICT = {
+    0   :"any",
+    1   :"unspecified",
+    2   :"video game",
+    3   :"anime",
+    4   :"rock",
+    5   :"pop",
+    6   :"other",
+    7   :"novelty",
+    9   :"hip hop", # note that there's no 8
+    10  :"electronic"
+}
 
-def find_osu_file(file_path):
-    if os.path.isdir(file_path):
-        for file in os.listdir(file_path):
-            if file[-4:] == ".osu":
-                return os.path.join(file_path, file)
-    return "" # failed to find file
+LANG_DICT = {
+    0   :"any",
+    1   :"other",
+    2   :"english",
+    3   :"japanese",
+    4   :"chinese",
+    5   :"instrumental",
+    6   :"korean",
+    7   :"french",
+    8   :"german",
+    9   :"swedish",
+    10  :"spanish",
+    11  :"italian"
+}
+
+def is_osz_zip(zip_name):
+    return zip_name[-4:] == ".osz"
 
 def read_osu_file(file_path):
     with open(file_path, mode='r', encoding="shift_jisx0213", errors="replace") as osuf:
-        data = {
-            Keys.VERSION:int(osuf.readline().split("osu file format v")[1]),
-            Keys.AUDIO_FILENAME:"",
-            Keys.TITLE:"",
-            Keys.TITLE_UNICODE:"",
-            Keys.ARTIST: "",
-            Keys.ARTIST_UNICODE: "",
-            Keys.CREATOR: "",
-            Keys.TAGS: "",
-            Keys.BEATMAP_ID: "",
-            Keys.BEATMAP_SET_ID: "",
-            Keys.BG_FILENAME:""
+        data_dict = {
+            KEYS.VERSION:int(osuf.readline().split("osu file format v")[1]),
+            KEYS.AUDIO_FILENAME: "",
+            KEYS.MODE: "",
+            KEYS.TITLE: "",
+            KEYS.TITLE_UNICODE: "",
+            KEYS.ARTIST: "",
+            KEYS.ARTIST_UNICODE: "",
+            KEYS.CREATOR: "",
+            KEYS.TAGS: "",
+            KEYS.BEATMAP_ID: "",
+            KEYS.BEATMAP_SET_ID: "",
+            KEYS.BG_FILENAME: ""
         }
-        if data[Keys.VERSION] >= 3:
-            key_pairs = get_keys_for_ver(data[Keys.VERSION])
+        if data_dict[KEYS.VERSION] >= 3:
+            key_pairs = get_keys_for_ver(data_dict[KEYS.VERSION])
             if not key_pairs:
-                print("Failed to retrieve key pairs for version:\t%s" % data[Keys.VERSION])
+                print("Failed to retrieve key pairs for version:\t%s" % data_dict[KEYS.VERSION])
                 return {}
             keys_index = 0
             key_pair = key_pairs[keys_index]
@@ -62,34 +87,49 @@ def read_osu_file(file_path):
                 if line.strip() == "[TimingPoints]":
                     break
                 elif key_pair[1] in line:
-                    data[key_pair[0]] = line[len(key_pair[1]):].strip()
+                    data_dict[key_pair[0]] = line[len(key_pair[1]):].strip()
                     keys_index += 1
                     if keys_index < len(key_pairs):
                         key_pair = key_pairs[keys_index]
                 elif "0,0," in line and (".jpg" in line or ".png" in line):
-                    data[Keys.BG_FILENAME] = line.split("\"")[1]
+                    data_dict[KEYS.BG_FILENAME] = line.split("\"")[1]
             if keys_index < len(key_pairs):
                 return {} # failed to read file
-        return data
+        return data_dict
+
+def find_and_read_osu_file(file_path):
+    target_file = r""
+    target_data = {}
+    if os.path.isdir(file_path):
+        for file in os.listdir(file_path):
+            if file[-4:] == ".osu":
+                target_file = os.path.join(file_path, file)
+                target_data = read_osu_file(target_file)
+                if target_data[KEYS.MODE] == 0: # script prioritizes standard mode
+                    return target_data, target_file
+    if target_data:
+        return target_data, target_file # no standard mode exists so return what has been found
+    return {}, target_file # script failed
 
 def get_keys_for_ver(ver):
     if ver <= 2:
         return [] # invalid version
     elif 3 <= ver <= 5:
-        return [(Keys.AUDIO_FILENAME,"AudioFilename:"), (Keys.TITLE,"Title:"), (Keys.ARTIST,"Artist:"),
-                (Keys.CREATOR,"Creator:")]
+        return [(KEYS.AUDIO_FILENAME, "AudioFilename: "), (KEYS.TITLE, "Title:"), (KEYS.ARTIST, "Artist:"),
+                (KEYS.CREATOR, "Creator:")]
     elif 6 <= ver <= 9:
-        return [(Keys.AUDIO_FILENAME,"AudioFilename:"), (Keys.TITLE,"Title:"), (Keys.ARTIST,"Artist:"),
-                (Keys.CREATOR,"Creator:"), (Keys.TAGS,"Tags:")]
+        return [(KEYS.AUDIO_FILENAME, "AudioFilename: "), (KEYS.MODE, "Mode: "), (KEYS.TITLE, "Title:"),
+                (KEYS.ARTIST, "Artist:"), (KEYS.CREATOR, "Creator:"), (KEYS.TAGS, "Tags:")]
     else:  # ver 10, 11, 12, 13, 14, or above
-        return [(Keys.AUDIO_FILENAME,"AudioFilename:"), (Keys.TITLE,"Title:"), (Keys.TITLE_UNICODE,"TitleUnicode:"),
-                (Keys.ARTIST,"Artist:"), (Keys.ARTIST_UNICODE,"ArtistUnicode:"), (Keys.CREATOR,"Creator:"),
-                (Keys.TAGS,"Tags:"), (Keys.BEATMAP_ID,"BeatmapID:"), (Keys.BEATMAP_SET_ID,"BeatmapSetID:")]
+        return [(KEYS.AUDIO_FILENAME, "AudioFilename: "), (KEYS.MODE, "Mode: "), (KEYS.TITLE, "Title:"),
+                (KEYS.TITLE_UNICODE, "TitleUnicode:"), (KEYS.ARTIST, "Artist:"), (KEYS.ARTIST_UNICODE, "ArtistUnicode:"),
+                (KEYS.CREATOR, "Creator:"), (KEYS.TAGS, "Tags:"), (KEYS.BEATMAP_ID, "BeatmapID:"),
+                (KEYS.BEATMAP_SET_ID, "BeatmapSetID:")]
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--songs_path", default=os.path.dirname(os.path.dirname(__file__)),
+    parser.add_argument("-p", "--songs_path", default="./", # parent directory is default
                         help="path to your osu songs folder (default is script's parent directory)")
     parser.add_argument("-k", "--key", default="",
                         help="your osu api key. adding this will provide extra metadata to your mp3 files "
@@ -101,61 +141,79 @@ if __name__ == "__main__":
         print("Provided path does not exist")
         exit(1)
     key = args.key
-    if key and not urllib.request.urlopen(r"https://osu.ppy.sh/api/get_beatmap?k={}".format(key)):
-        print("Provided key is invalid")
-        exit(1)
+    if key:
+        try:
+            request.urlopen(r"https://osu.ppy.sh/api/get_beatmaps?k={}".format(key))
+        except url_error.URLError:
+            print("Could connect to osu api")
+            exit(1)
     # Create temp path to store extracted folders
-    temp_path = os.path.join(os.path.dirname(__file__) + r"\temp")
+    temp_path = os.path.join( songs_path, "temp")
     if not os.path.exists(temp_path):
         os.mkdir(temp_path)
     # Create folder to store formatted mp3's
-    formatted_dest = os.path.join(os.path.dirname(__file__), r"\formatted_mp3")
+    formatted_dest = os.path.join( songs_path, "formatted_mp3")
     if not os.path.exists(formatted_dest):
         os.mkdir(formatted_dest)
     # Main loop
     for directory in os.listdir(songs_path):
-        # Find the beatmap set ID
+        # Find .osz zip file
         song_path = os.path.join(songs_path, directory)
-        if not os.path.exists(song_path):
-            print("Failed to find path to the following directory:\t%s" % directory)
-            continue
         if not zipfile.is_zipfile(song_path) or not is_osz_zip(directory):
             continue # not a file of interest
+        # Extract .osz zip file
         with open(song_path, 'rb') as zf:
             z = zipfile.ZipFile(zf)
             extract_to = os.path.join(temp_path, os.path.basename(song_path)[:-4])
+            if not os.path.exists(extract_to):
+                os.mkdir(extract_to)
             z.extractall(extract_to)
-        osu_file = find_osu_file(extract_to)
+        # Find and read .osu file
+        file_data, osu_file = find_and_read_osu_file(extract_to)
         if not osu_file:
             print("Failed to find .osu file in:\t%s" % song_path)
             continue
-        file_data = read_osu_file(osu_file)
         if not file_data:
             print("Failed to read .osu file at:\t%s" % osu_file)
             continue
-        split = directory.split()
-        if split[0].isdigit():
-            file_data[Keys.BEATMAP_SET_ID] = split[0]
-        if not file_data[Keys.BEATMAP_SET_ID]:
+        # If .osu file version < 10 find set ID in zip file name
+        if file_data[KEYS.VERSION] < 10:
+            split = directory.split()
+            if split[0].isdigit():
+                file_data[KEYS.BEATMAP_SET_ID] = split[0]
+        if not file_data[KEYS.BEATMAP_SET_ID]:
             print("Failed to find beatmap set ID for:\t%s" % song_path)
             continue
         # Find mp3, copy it, rename it, edit metadata
-        mp3_file_path = os.path.join(song_path, file_data[Keys.AUDIO_FILENAME])
-        if not os.path.exists(formatted_dest):
-            os.makedirs(formatted_dest)
-        mp3_dest = os.path.join(formatted_dest, file_data[Keys.ARTIST] + " - " + file_data[Keys.TITLE] + ".mp3")
+        mp3_file_path = os.path.join(extract_to, file_data[KEYS.AUDIO_FILENAME])
+        copy_mp3_name = (file_data[KEYS.ARTIST] + " - " + file_data[KEYS.TITLE] + ".mp3")\
+            .replace(':', "_").replace('\\','-')
+        mp3_dest = os.path.join(formatted_dest, copy_mp3_name)
         copyfile(mp3_file_path, mp3_dest)
         # Load mp3, give it a tag if it doesn't already exists
         audioFile = eyed3.load(mp3_dest)
-        if audioFile.tag is None:
-            audioFile.tag = eyed3.id3.Tag()
-            audioFile.tag.file_info = eyed3.id3.FileInfo(mp3_dest)
+        audioFile.tag = eyed3.id3.Tag()
+        audioFile.tag.file_info = eyed3.id3.FileInfo(mp3_dest)
+        # Add genre and language if api key was given
+        comment = ""
+        if key:
+            with request.urlopen(r"https://osu.ppy.sh/api/get_beatmaps?k={}&s={}"
+                                                .format(key, file_data[KEYS.BEATMAP_SET_ID])) as response:
+                data = json.loads(response.read())[0]
+            audioFile.tag.genre = GENRE_DICT[int(data["genre_id"])]
+            comment += "Language: " + LANG_DICT[int(data["language_id"])] + " "
         # Add front cover image
-        if file_data[Keys.BG_FILENAME]:
-            bg_file_path = os.path.join(song_path, file_data[Keys.BG_FILENAME])
+        if file_data[KEYS.BG_FILENAME]:
+            bg_file_path = os.path.join(extract_to, file_data[KEYS.BG_FILENAME])
             imageData = open(bg_file_path, "rb").read()
             audioFile.tag.images.set(3, imageData, mime_type="")
         # Add other metadata and save
-        audioFile.tag.artist = file_data[Keys.ARTIST]
-        audioFile.tag.title = file_data[Keys.TITLE]
+        audioFile.tag.artist = file_data[KEYS.ARTIST]
+        audioFile.tag.title = file_data[KEYS.TITLE]
+        audioFile.tag.comments.set(comment + "Tags: " + file_data[KEYS.TAGS])
         audioFile.tag.save()
+    # Delete temp directory
+    shutil.rmtree(temp_path)
+
+__all__ = ['argparse', 'os', 'shutil', 'eyed3', 'logging', 'zipfile',
+           'json', 'copyfile', 'Enum', 'request', 'url_error', 'id3']
